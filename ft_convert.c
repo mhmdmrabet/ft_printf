b50324d97cb0a9902759ba960cb4b2184698fc7c
Mohamed Mrabet <mmrabet@e1r4p16.clusters.42paris.fr>
2022-11-17 14:21:16 +0100

    ft_printf DONE

--- ft_convert.c
+++ ft_convert.c
@@ -12,6 +12,14 @@
 
 #include "ft_printf.h"
 
+void	ft_convert_all_hexa(const char *s, va_list ap, t_printf *data)
+{
+	if (*s == 'X')
+		ft_convert_hexa(va_arg(ap, int), "0123456789ABCDEF", data);
+	else if (*s == 'x')
+		ft_convert_hexa(va_arg(ap, int), "0123456789abcdef", data);
+}
+
 void	ft_convert(const char *s, va_list ap, t_printf *data)
 {
 	data->nb_params = ft_search_params(s);
@@ -21,17 +29,20 @@ void	ft_convert(const char *s, va_list ap, t_printf *data)
 			ft_putchar_len(*s, data);
 		if (*s == '%')
 		{
-			s++;
-			if (*s == 'c' || *s == 's' || *s == '%')
-				ft_convert_str(s, data, ap);
-			else if (*s == 'd' || *s == 'i' || *s == 'u')
-				ft_convert_nbr(s, data, ap);
-			else if (*s == 'p')
-				ft_convert_ptr(data, ap);
-			else if (*s == 'X')
-				ft_convert_hexa(va_arg(ap, int), "0123456789ABCDEF", data);
-			else if (*s == 'x')
-				ft_convert_hexa(va_arg(ap, int), "0123456789abcdef", data);
+			if (ft_is_type(*(s + 1)))
+			{
+				s++;
+				if (*s == 'c' || *s == 's' || *s == '%')
+					ft_convert_str(s, data, ap);
+				else if (*s == 'd' || *s == 'i' || *s == 'u')
+					ft_convert_nbr(s, data, ap);
+				else if (*s == 'p')
+					ft_convert_ptr(data, ap);
+				else if (*s == 'X' || *s == 'x')
+					ft_convert_all_hexa(s, ap, data);
+			}
+			else
+				ft_putchar_len(*s, data);
 		}
 		s++;
 		data->nb_params--;
